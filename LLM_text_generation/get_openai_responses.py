3a296178cae2049d4ffefa060ad480f1ef9e7078
lukatercon <luka.tercon@gmail.com>
2025-10-27 11:31:31 +0100

    Additional adjustments before first data generation

---
+++ LLM_text_generation/get_openai_responses.py
@@ -0,0 +1,99 @@
+import os
+from openai import OpenAI
+
+
+def combine_with_Šolar_template(title_info, length):
+    title_string = ""
+    subtitle_string = ""
+    ref_lit_work_string = ""
+    
+    # account for cases in which some of the information about the title is missing
+    if title_info[0]:
+        title_string = f" z naslovom {title_info[0]}"
+    
+    if title_info[2]:
+        ref_lit_work_string = f", ki se nanaša na literarno delo {title_info[2]},"
+    
+    if title_info[1]:
+        if title_info[0]:
+            subtitle_string = f" in podnaslovom {title_info[1]}"
+        else:
+            subtitle_string = f" s podnaslovom {title_info[1]}"
+
+    template = f"Napiši esej{title_string}{subtitle_string}{ref_lit_work_string} s približno {str(length)} besedami. Odgovori samo z esejem brez spremnega besedila."
+
+    return template
+
+
+def build_lengths_dict(lengths_file):
+    len_dict = dict()
+    with open(lengths_file, "r", encoding="utf-8") as rf_len:
+        for line in rf_len.readlines()[1:]:
+            doc_id, length = line.strip().split("\t")
+            len_dict[doc_id] = int(length)
+
+    return len_dict
+
+
+def build_titles_dict(titles_file):
+    titles_dict = dict()
+    with open(titles_file, "r", encoding="utf-8") as rf_titles:
+        for line in rf_titles.readlines()[1:]:
+            doc_id, title, ref_work, subtitle = line.strip().split("\t")
+            
+            title = title if title != "N/A" else None
+            ref_work = ref_work if ref_work != "N/A" else None
+            subtitle = subtitle if subtitle != "N/A" else None
+            titles_dict[doc_id] = (title, subtitle, ref_work)
+    
+    return titles_dict
+
+# define the client and the model to be used
+#client = OpenAI()
+model_to_use = "gpt5-chat-latest"     # this is the model used by default in the ChatGPT browser interface
+
+# define the files that contain lengths and titles
+lengths_file = os.path.join("..", "Datasets", "Solar", "Solar_lengths.tsv")
+titles_file = os.path.join("..", "Datasets", "Solar", "Solar_annotated_titles.tsv")
+
+# define the output dir
+output_dir = os.path.join("..", "Datasets", "Solar", "Solar_GPT-5", "raw")
+
+
+# build text lengths dictionary and titles dictionary
+len_dict = build_lengths_dict(lengths_file)
+titles_dict = build_titles_dict(titles_file)
+
+# go through every document in human-written and generate a corresponding machine generated essay, 
+# but only if at least the title or the referenced literary work of the essay is known
+i = 1
+for doc_id in titles_dict.keys():
+    title_info = titles_dict[doc_id]
+
+    if title_info[0] or title_info[2]: 
+        print(doc_id)
+
+        """
+        prompt = combine_with_Šolar_template(title_info, len_dict[doc_id])
+
+        completion = client.chat.completions.create(
+            model=model_to_use,
+            temperature=1,
+            top_p=1,
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},  # use a default system prompt in English
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ]
+        )
+
+        with open(os.path.join(output_dir, f"{doc_id}.txt"), "w", encoding="utf-8") as wf:
+            wf.write(completion.choices[0].message.content)
+
+        if i % 100 == 0:
+            print(f"Document {i}/{len(titles_dict.keys())}")
+
+        i += 1"""
+
--- README.md
+++ README.md
@@ -7,8 +7,14 @@ Prompts used: see LLM/text_generation/text_generation_prompts.txt
 
 ### Šolar
 
+Process summary: 
+We employed a human annotator to find existing explicitly mentioned titles within the Šolar corpus and used those to build prompts for generating MGT using LLMs (see below for a list). The prompt template is stored in LLM_text_generation/text_generation_prompts.md. We generated a corresponding MG essay only for those texts from Šolar that:
+
+1. are labeled as "esej ali spis" in the corpus metadata
+2. have an explicitly mentioned essay title or at least explicitly refer to some literary work as the basis for the content. This ensures that the content of the MG essays stays as close to the HW essays as possible.
+
 Models used:
 - GPT-5 - the default ChatGPT model, currently still a very widely used AI text generation platform
 
 Grammatical annotation tools used:
-- Trankit - using the model retrained on the SSJ UD and SST UD treebanks v2.15: [https://www.clarin.si/repository/xmlui/handle/11356/1997](https://www.clarin.si/repository/xmlui/handle/11356/1997). We chose this model, since it achieves SOTA performance on the UD dependency parsing task, which is crucial for studies of syntax.
+- Trankit - using the model retrained on the SSJ UD and SST UD treebanks v2.15: [https://www.clarin.si/repository/xmlui/handle/11356/1997](https://www.clarin.si/repository/xmlui/handle/11356/1997). We chose this model to annotate the Slovenian texts, since it achieves SOTA performance on the UD dependency parsing task, which is crucial for studies of syntax.
